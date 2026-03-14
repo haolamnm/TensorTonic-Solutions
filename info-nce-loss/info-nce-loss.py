875e6cb144ae5f1f2ffe7df76e789d15ee68b653
Hao Lam <haolamnm@gmail.com>
2026-03-14 16:21:24 +0700

    Update solution for info-nce-loss

--- info-nce-loss/info-nce-loss.py
+++ info-nce-loss/info-nce-loss.py
@@ -4,9 +4,9 @@ def stable_softmax(X):
     # [min(X), max(X)]
     # [min(X) - max(X), 0]
     # exp([min(X) - max(X), 0]) -> [0, 1]
-    Z = np.exp(X - np.max(X))
+    Z = np.exp(X - np.max(X, axis=1, keepdims=True))
     N, _ = Z.shape
-    P = Z / np.sum(Z, axis=1)
+    P = Z / np.sum(Z, axis=1, keepdims=True)
 
     # scores -> probs
     return P
@@ -23,6 +23,7 @@ def info_nce_loss(Z1, Z2, temperature=0.1):
 
     # take only (i, i) pair
     P = np.diag(stable_softmax(S))
+    P_safe = np.clip(P, 1e-12, 1.0)
 
     L = -np.mean(np.log(P))
     return L
