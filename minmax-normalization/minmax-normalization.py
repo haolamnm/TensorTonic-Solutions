cfad56abacb2cd39a63c80f69f7fe93483048dc3
Hao Lam <haolamnm@gmail.com>
2026-03-18 15:51:48 +0700

    Update solution for minmax-normalization

--- minmax-normalization/minmax-normalization.py
+++ minmax-normalization/minmax-normalization.py
@@ -6,14 +6,7 @@ def minmax_scale(X, axis=0, eps=1e-12):
     Return np.ndarray (float).
     """
     X = np.asarray(X)
-    X_min = np.min(X, axis=axis)
-    X_max = np.max(X, axis=axis)
-    print("max", X_max)
-    print("min", X_min)
-    if X.ndim == 2 and axis == 1:
-        X_new = (X.T - X_min) / (X_max - X_min + eps)
-        X_new = X_new.T
-    else:
-        X_new = (X - X_min) / (X_max - X_min + eps)
-    print("new", X_new)
+    X_min = np.min(X, axis=axis, keepdims=True)
+    X_max = np.max(X, axis=axis, keepdims=True)
+    X_new = (X - X_min) / (X_max - X_min + eps)
     return X_new
