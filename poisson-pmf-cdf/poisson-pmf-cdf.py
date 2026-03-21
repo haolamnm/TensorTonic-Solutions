7932430dceacfcd2d6e7c38b9afb45b98b08dc7e
Hao Lam <haolamnm@gmail.com>
2026-03-21 22:22:58 +0700

    Update solution for poisson-pmf-cdf

--- poisson-pmf-cdf/poisson-pmf-cdf.py
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -6,7 +6,13 @@ def poisson_pmf_cdf(lam, k):
     Compute Poisson PMF and CDF.
     """
     indices = np.arange(k+1)
-    factorial = np.vectorize(math.factorial)
-    probs = np.exp(-lam) * np.power(lam, indices) / factorial(indices)
+
+    # log(k!) = sum(log(1), log(2), ..., log(k))
+    log_factorial = np.zeros(k + 1)
+    if k > 0:
+        log_factorial[1:] = np.cumsum(np.log(indices[1:]))
+
+    log_probs = -lam + indices * np.log(lam) - log_factorial
+    probs = np.exp(log_probs)
 
     return probs[k], probs.sum()
