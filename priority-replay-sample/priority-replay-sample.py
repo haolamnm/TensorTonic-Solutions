3a20d87db9a9f0a4d3fe19bd05c5aa909af7dbfc
Hao Lam <haolamnm@gmail.com>
2026-03-22 19:40:24 +0700

    Update solution for priority-replay-sample

--- priority-replay-sample/priority-replay-sample.py
+++ priority-replay-sample/priority-replay-sample.py
@@ -6,17 +6,13 @@ def priority_replay_sample(priorities, alpha, beta):
     """
     priorities = np.asarray(priorities, dtype=float)
     powered_priorities = priorities ** alpha
-    print(powered_priorities)
 
     probs = powered_priorities / np.sum(powered_priorities)
-    print(probs)
 
     batch_size = len(probs)
     weights = (batch_size * probs) ** (-beta)
-    print(weights)
 
     weights_hat = weights / np.max(weights)
-    print(weights_hat)
 
     return [probs.tolist(), weights_hat.tolist()]
     
