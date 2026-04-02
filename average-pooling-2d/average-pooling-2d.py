0e39e91a882e9fa60aecd847db8fb105745dc48f
Hao Lam <haolamnm@gmail.com>
2026-04-02 22:02:06 +0700

    Add solution for average-pooling-2d

---
+++ average-pooling-2d/average-pooling-2d.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def average_pooling_2d(X, pool_size):
+    """
+    Apply 2D average pooling with non-overlapping windows.
+    """
+    X = np.asarray(X)
+    p = pool_size
+    H, W = X.shape
+    H_out, W_out = H // p, W // p
+
+    out = np.zeros((H_out, W_out))
+    
+    for i in range(H_out):
+        for j in range(W_out):
+            # slice the p x p window directly
+            window = X[i*p : i*p+p, j*p : j*p+p]
+            out[i, j] = np.mean(window)
+            
+    return out.tolist()
+    
