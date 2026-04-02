377ceca8e32660fec861470c4de5db62a14a20bd
Hao Lam <haolamnm@gmail.com>
2026-04-02 22:24:44 +0700

    Add solution for gaussian-blur-kernel

---
+++ gaussian-blur-kernel/gaussian-blur-kernel.py
@@ -0,0 +1,22 @@
+import math as m
+
+def gaussian_kernel(size, sigma):
+    """
+    Generate a normalized 2D Gaussian blur kernel.
+    """
+    center = size // 2
+    G = [[0 for _ in range(size)] for _ in range(size)]
+
+    for i in range(size):
+        for j in range(size):
+            y = i - center
+            x = j - center
+
+            G[i][j] = m.e ** (-(x**2 + y**2) / (2 * sigma**2))
+
+    S = sum(sum(row) for row in G)
+    for i in range(size):
+        for j in range(size):
+            G[i][j] /= S
+
+    return G
