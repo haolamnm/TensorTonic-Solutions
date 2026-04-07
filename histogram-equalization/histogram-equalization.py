286de6545913624998e3d3f48a975ef489254208
Hao Lam <haolamnm@gmail.com>
2026-04-07 21:19:46 +0700

    Update solution for histogram-equalization

--- histogram-equalization/histogram-equalization.py
+++ histogram-equalization/histogram-equalization.py
@@ -10,13 +10,11 @@ def histogram_equalize(image):
     total_pixels = len(flat)
     
     cdf = np.cumsum(bins)
-    print(cdf)
-
     cdf_min = np.min(cdf[cdf != 0])
-    print(cdf_min)
 
     if total_pixels == cdf_min:
         return np.zeros_like(image).tolist()
 
+    # clean subscript ops
     new_val = np.round((cdf[flat] - cdf_min) / (total_pixels - cdf_min) * 255).reshape(image.shape)
     return new_val.tolist()
