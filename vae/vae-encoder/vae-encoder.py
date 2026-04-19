cf3eb53d434e2d9bcc39f7358378de85dfd28427
Hao Lam <haolamnm@gmail.com>
2026-04-19 23:02:47 +0700

    Add solution for vae-encoder

---
+++ vae/vae-encoder/vae-encoder.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def vae_encoder(x: np.ndarray, W_mu: np.ndarray, b_mu: np.ndarray, W_logvar: np.ndarray, b_logvar: np.ndarray) -> dict:
+    """
+    Returns: dict with 'mu' and 'log_var' as np.ndarrays of shape (batch, latent_dim)
+    """
+    mu = x @ W_mu + b_mu
+    logvar = x @ W_logvar + b_logvar
+    return (mu, logvar)
