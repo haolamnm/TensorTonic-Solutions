763ba4fa38f95645a6b21692a425e4e31b8695b2
Hao Lam <haolamnm@gmail.com>
2026-04-21 23:27:59 +0700

    Add solution for alexnet-dropout

---
+++ alexnet/alexnet-dropout/alexnet-dropout.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def dropout(x: np.ndarray, p: float = 0.5, training: bool = True, mask: np.ndarray = None) -> np.ndarray:
+    """
+    Apply inverted dropout. If mask is provided, use it; otherwise generate one.
+    """
+    if not training:
+        return x
+    
+    if mask is None:
+        mask = np.random.binomial(1, 1- p, size=x.shape)
+
+    return x * mask * (1 / (1 - p))
